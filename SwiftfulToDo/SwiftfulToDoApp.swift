fa9bbe68f2c591e7e2af8c1445c1743ecbd0615a
Tony <tong.decula@gmail.com>
2023-03-16 14:08:51 -0400

    feat: init the project.

---
+++ SwiftfulToDo/SwiftfulToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulToDoApp.swift
+//  SwiftfulToDo
+//
+//  Created by Tony on 2023-03-16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
